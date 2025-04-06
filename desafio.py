3262a246f3d7b6ec610a91545794fe0fed2ab0dc
Diogo Souza <diogodsribeiro@gmail.com>
2025-04-06 19:33:24 -0300

    Adicionar desafio

---
+++ desafio.py
@@ -0,0 +1,11 @@
+contante_bonus = 1000
+
+user_name = input('Digite o seu nome: ')
+
+user_salary = float(input('digite o valor do seu salário: '))
+
+bonus_user = contante_bonus
+
+print(f'total value: {user_salary + bonus_user}')
+
+print(f'Name: {user_name}\nsalary: {user_salary}\nbonus: {bonus_user}')
