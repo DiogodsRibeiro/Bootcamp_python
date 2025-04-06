126682466aa01ed822cc5312757e160107f20286
Diogo Souza <diogodsribeiro@gmail.com>
2025-04-06 19:36:50 -0300

    Incluindo exemplo 02

---
+++ exemplo_02.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuario digite dois valores e apareça a soma
+
+Valor1 = int(input('Digite o primeiro numero: '))
+valor2 = int(input('Digite o segundo numero: '))
+
+print(f'O resultado é: {Valor1 + valor2} ')
